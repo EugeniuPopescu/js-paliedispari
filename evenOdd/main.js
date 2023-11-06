2a6e51f68fa27136c9c0ac7cb1c58b2692707593
Eugen <epopescuu@yahoo.it>
2023-11-06 16:51:21 +0100

    first commit

---
+++ evenOdd/main.js
@@ -0,0 +1,43 @@
+// faccio scegliere se pari o dispari 
+let choice = prompt('Scegli Pari o Dispari (se non scegli ti verra assegnato automaticamente Pari) ', 'pari');
+
+if(choice != "pari" && choice != "dispari") {
+    alert('Errore. Abbiamo scelto per te: punterai sui pari');
+    choice = "pari";
+}
+
+const io = parseInt(prompt('Insierisci numero (tra 1 e 5)'));
+
+if (io  > 5 || io < 1) {
+    alert('Inserisci un numero tra 1 e 5 compresi');
+}
+
+// funzione numero random da 1 a 5 per il pc
+function randomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+const pc = randomNumber(1, 5);
+
+const sum = io + pc;
+
+function evenOdd() {
+    let result;
+    let outcome;
+
+    if (sum % 2 == 0) {
+        result = 'pari';
+    } else {
+        result = 'dispari';
+    }
+
+    if (choice == result) {
+        outcome = 'You Win';
+    } else {
+        outcome = 'You Lost';
+    }
+
+    return outcome;
+}
+console.log(io, pc);
+console.log(evenOdd());
