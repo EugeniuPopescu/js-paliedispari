cf57712709d6b0b210888267f2d40265047500d6
Eugen <epopescuu@yahoo.it>
2023-11-06 18:16:30 +0100

    fix: add controll to evenOdd

--- evenOdd/main.js
+++ evenOdd/main.js
@@ -2,44 +2,56 @@
 let choice = prompt('Scegli Pari o Dispari (se non scegli ti verra assegnato automaticamente Pari) ', 'pari');
 
 if(choice != "pari" && choice != "dispari") {
-    alert('Errore. Abbiamo scelto per te: punterai sui pari');
+    alert('Errore. Abbiamo scelto per te: pari');
     choice = "pari";
 }
-
 const io = parseInt(prompt('Insierisci numero (tra 1 e 5)'));
 
-if (io  > 5 || io < 1) {
-    alert('Inserisci un numero tra 1 e 5 compresi');
-}
+// controllo se è stato inserito un numero
+if (!isFinite(io)) {
 
-// funzione numero random da 1 a 5 per il pc
-function randomNumber(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) ) + min;
-}
+    // messaggio di errore
+    document.querySelector('#outcome').innerHTML = `Non hai inserito un numero!`;
+    
+} else {
 
-const pc = randomNumber(1, 5);
-
-const sum = io + pc;
-
-function evenOdd() {
-    let result;
-    let outcome;
-
-    if (sum % 2 == 0) {
-        result = 'pari';
+    // controllo se il numero che è stato inserito è compreso tra 1 e 5
+    if (io  > 5 || io < 1) {
+        // do messaggio di errore
+        document.querySelector('#outcome').innerHTML = `Hai inserito un numero troppo grande! (${io})`;
     } else {
-        result = 'dispari';
-    }
 
-    if (choice == result) {
-        outcome = 'Hai Vinto';
-    } else {
-        outcome = 'Hai Perso';
+        // funzione numero random da 1 a 5 per il pc
+        function randomNumber(min, max) {
+            return Math.floor(Math.random() * (max - min + 1) ) + min;
+        }
+        
+        const pc = randomNumber(1, 5);
+        
+        const sum = io + pc;
+        
+        function evenOdd() {
+            let result;
+            let outcome;
+        
+            if (sum % 2 == 0) {
+                result = 'pari';
+            } else {
+                result = 'dispari';
+            }
+        
+            if (choice == result) {
+                outcome = 'Hai Vinto';
+            } else {
+                outcome = 'Hai Perso';
+            }
+        
+            return outcome;
+        }
+        
+        document.querySelector('#io').innerHTML = `Il tuo numero: ${io}`;
+        document.querySelector('#pc').innerHTML = `PC's Number: ${pc}`;
+        document.querySelector('#outcome').innerHTML = `Hai Scelto ${choice.toUpperCase()}, somma: ${sum} => ${evenOdd()}`;
     }
-
-    return outcome;
 }
 
-document.querySelector('#io').innerHTML = `Il tuo numero: ${io}`;
-document.querySelector('#pc').innerHTML = `PC's Number: ${pc}`;
-document.querySelector('#outcome').innerHTML = `Hai Scelto ${choice.toUpperCase()}, somma: ${sum} => ${evenOdd()}`;
