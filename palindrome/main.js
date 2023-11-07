8c803da38983157ad4b70ca36b7e6b90f5fd8ba9
Eugen <epopescuu@yahoo.it>
2023-11-07 11:27:54 +0100

    perf:(palindrome) add boolean variable and comment

--- palindrome/main.js
+++ palindrome/main.js
@@ -1,23 +1,35 @@
+// recupero la parola dall'utente
 let word = prompt('Inserisci una Parola:');
+
+// rigiro la parola al contrario attraverso la mia funzione palindrome();
 let reversedWord = palindrome(word);
 
-function palindrome(word) {
+// controllo la parola sia uguale al contrario del suo risultato
+isPalindrome = false;
+
+if (word == reversedWord) {
+    isPalindrome = true;    
+} 
+
+// output: avvisuo l'utente
+if (isPalindrome) {
+    document.querySelector('#io').innerHTML = `La parola "${word}" è Palindroma`;
+} else {
+    document.querySelector('#io').innerHTML = `La parola "${word}" NON è Palindroma`;
+}
+
+// la x non deve essere dichiarata, posso scrivere quello che voglio nei parametri della funzione
+function palindrome(x) {
     let reversed = '';
 
     // capovolgo la parola
-    for (let i = word.length - 1; i >= 0; i--) {
-        reversed += word[i];
+    for (let i = x.length - 1; i >= 0; i--) {
+        reversed += x[i];
     }
 
     return reversed;
 }
 
-
-if (word == reversedWord) {
-    document.querySelector('#io').innerHTML = `La parola "${word}" è Palindroma`;
-} else {
-    document.querySelector('#io').innerHTML = `La parola "${word}" NON è Palindroma`;
-}
 
 // let word = prompt('Inserisci una Parola:');
 
@@ -28,3 +40,5 @@ if (word == reversedWord) {
 
 // }
 // console.log(reversed);
+
+
